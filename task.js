53ebcddd9fb8db9fbad5167ebbe38c2f939d528d
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2022-05-12 20:24:22 -0300

    feat(search): add function that fetches all tasks in database

--- task.js
+++ task.js
@@ -41,3 +41,34 @@ function getSingleTaskById(getTaskId) {
     db.connection.end()
   })
 }
+
+function getAllTasks() {
+  db.connection.connect(async function (err) {
+    try {
+      let rowAllFoundTasks = []
+      rowAllFoundTasks = await query('SELECT * FROM todo')
+
+      if (rowAllFoundTasks != 0) {
+        console.log('\n===== TAREFAS =====')
+        for (let index = 0; index < rowAllFoundTasks.length; index++) {
+          console.log(
+            `\n==== Tarefa ${index + 1} ====\n` +
+              '\nConteúdo da tarefa:\n ' +
+              rowAllFoundTasks[index].content +
+              '\nStatus da tarefa:\n ' +
+              rowAllFoundTasks[index].isDone
+          )
+        }
+      } else {
+        console.log(
+          '\nNão existe tarefas no banco de dados... Tente novamente...'
+        )
+      }
+    } catch (error) {
+      console.log('Não há conexão com o banco de dados...', error.message)
+      throw err
+    } finally {
+      db.connection.end()
+    }
+  })
+}
