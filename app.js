fc50baa56fde306e4f3536ecf2a2b62233c35431
Gabriel D'Nillo <gabrieldnillo99@gmail.com>
2022-05-17 19:18:58 -0300

    feat(menu): add functions to the main menu.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function printMainMenu() {
+const read = require('readline-sync')
+const tasks = require('./task')
+
+function mainMenu() {
   const title = '\n===== PCD - TODO LIST ====='
   const author = "===== Gabriel D'Nillo =====\n"
   const menuOptions = [
@@ -13,10 +16,38 @@ function printMainMenu() {
   console.log(title)
   console.log(author)
 
-  console.log('Escolha uma opção:\n')
   for (let index = 0; index < menuOptions.length; index++) {
     console.log(`${index + 1}. ${menuOptions[index]}.`)
   }
+
+  input = read.questionInt('\nEscolha uma opção:\n', {
+    limitMessage:
+      'Desculpe, esse campo aceita apenas números inteiros de 1 até 6... Tente novamente...'
+  })
+  switch (input) {
+    case 1:
+      tasks.createTask()
+      break
+    case 2:
+      tasks.updateTask()
+      break
+    case 3:
+      tasks.getAllTasks()
+      break
+    case 4:
+      tasks.getSingleTaskById()
+      break
+    case 5:
+      tasks.deleteTaskById()
+      break
+    case 6:
+      console.log('O programa fechou...')
+      process.exit()
+    default:
+      console.log(
+        'Desculpe, esse campo aceita apenas números inteiros de 1 até 6... Tente novamente...'
+      )
+  }
 }
 
-printMainMenu()
+mainMenu()
